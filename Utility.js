49851e69cd518eb51b5a7f885df8322206b63898
tn16jv <tn16jv@brocku.ca>
2019-06-13 07:46:30 -0400

    Response link is now appended.

--- Utility.js
+++ Utility.js
@@ -14,7 +14,7 @@ function postURL() {
         data:'longuri=' + urlString,
         type: "POST",
         success:function(data){
-            $("#ajaxArea").html(data);
+            $("#ajaxArea").append(data);
         },
         error:function (){alert("failure");},
         async: true
